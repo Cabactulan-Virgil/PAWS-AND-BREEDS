da47d2132de73379fa16b5d44808f9ae7fb9f0b2
Nazef Hawk Lague <112536913+Cabactulan-Virgil@users.noreply.github.com>
2024-11-17 11:49:29 +0800

    include php function

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// Database connection
+$servername = "localhost:3306";
+$username = "root";
+$password = "";
+$dbname = "pawsandbreeds";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+?>
---
+++ handler.js
@@ -0,0 +1,16 @@
+///Login Forms Section Handler
+document.getElementById("Lexitbtnnn").addEventListener("click", function() {
+    document.getElementById("LoginAreaaa").style.display = "none";
+});
+document.getElementById("login-now-btn").addEventListener("click", function() {
+    document.getElementById("LoginAreaaa").style.display = "flex";
+});
+
+///Register Forms Section Handler
+document.getElementById("Rexitbtnnn").addEventListener("click", function() {
+    document.getElementById("RegisterAreaaa").style.display = "none";
+});
+document.getElementById("register-now-btn").addEventListener("click", function() {
+    document.getElementById("RegisterAreaaa").style.display = "flex";
+});
+
