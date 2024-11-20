d7dc1da733ed70a805fd0b2cef83a79c1b548e42
W4K312 <112536913+Cabactulan-Virgil@users.noreply.github.com>
2024-11-21 04:42:51 +0800

    login & back end side function

--- heroslider.js
+++
@@ -1,7 +0,0 @@
-
-document.getElementById("exitbtnnn").addEventListener("click", function() {
-    document.getElementById("LoginAreaaa").style.display = "none";
-});
-document.getElementById("login-now-btn").addEventListener("click", function() {
-    document.getElementById("LoginAreaaa").style.display = "flex";
-});
